9452d0edeab4cb9ed7a8260798c82c62b0e2fee3
Avinash Patil <eternaldenycries@gmail.com>
2018-10-17 21:19:33 -0700

    calculating distance at once and then varying k

--- load_dataset.py
+++ load_dataset.py
@@ -5,6 +5,7 @@ import matplotlib as mpl
 from matplotlib import pyplot
 from Log_Reg import LogisticRegressionOVA
 from KNN import KNN
+import time
 
 
 def read(data_set="training", path="."):
@@ -49,10 +50,12 @@ def log_reg_driver(images_train, labels_train, images_test, labels_test):
     scores = []
     for n_iter in iterations:
         print("Number of iterations: ", n_iter)
-        logi = LogisticRegressionOVA(n_iter).fit(images_train, labels_train)
+        logi = LogisticRegressionOVA(0.00001, n_iter)
+        logi.fit(images_train, labels_train)
         # logi = LogisticRegressionOVA(n_iter).fit(images_train[:30000:], labels_train[:30000:])
         scores.append(logi.score(images_test, labels_test))
 
+    print(scores)
     pyplot.plot(iterations, scores, 'r--')
     pyplot.savefig('log_reg.png')
     pyplot.show()
@@ -62,39 +65,47 @@ def knn_driver(images_train, labels_train, images_test, labels_test):
     knn = KNN(images_train, labels_train)
     batch_size = 200
     batches = len(images_test) // batch_size
+
+    dists = []
+    for i in range(batches):
+        print(str(i + 1) + "/" + str(len(images_test) // batch_size))
+        dists.extend(knn.compute_distances(images_test[i * batch_size:(i + 1) * batch_size]))
+
+    dists = np.array(dists)
+
     ks = [1, 3, 5, 10, 30, 50, 70, 80, 90, 100]
     # ks = [1, 10, 50, 70, 90, 100]
-    ks = [100]
+    # ks = [100]
     accuracy = []
-    for k in ks:
-        print("For K = ",k)
-        predictions = []
+    predictions = []
 
-        for i in range(batches):
-            print(str(i + 1) + "/" + str(len(images_test) // batch_size))
-            prediction = knn.predict(images_test[i * batch_size:(i + 1) * batch_size], k)
-            predictions = predictions + list(prediction)
+    for k in ks:
+        print("For K = ", k)
+        prediction = knn.predict(dists, k)
+        predictions = predictions + list(prediction)
 
         print("Completed predicting the test data.")
 
         score = 0
-        for i in range(len(predictions)):
+        for i in range(len(labels_test)):
             if int(labels_test[i]) == int(predictions[i]):
                 score += 1
         precision = score / len(labels_test)
         accuracy.append(precision)
 
         print("accuracy: ", precision)
+        predictions = []
 
-    out_file = open("predictions.csv", "w")
-    out_file.write("ImageId,Label\n")
-    for i in range(len(predictions)):
-        out_file.write(str(int(labels_test[i])) + "," + str(int(predictions[i])) + "\n")
-    out_file.close()
+
+    # out_file = open("predictions.csv", "w")
+    # out_file.write("ImageId,Label\n")
+    # for i in range(len(predictions)):
+    #     out_file.write(str(int(labels_test[i])) + "," + str(int(predictions[i])) + "\n")
+    # out_file.close()
 
     pyplot.plot(ks, accuracy, 'r--')
     pyplot.savefig('knn.png')
-    pyplot.show()
+    # pyplot.show()
 
 
 def main():
@@ -103,13 +114,19 @@ def main():
 
     print('Done loading data!')
 
-    print('Starting KNN')
-    knn_driver(images_train, labels_train, images_test, labels_test)
-    print('Done KNN!')
+    # print('Starting KNN')
+    # start_time = time.time()
+    # knn_driver(images_train, labels_train, images_test, labels_test)
+    # print('Done KNN!')
+    # end_time = time.time()
+    # print("Elapsed time was %g minutes" % ((end_time - start_time)/60))
 
     print('Starting Regression..')
+    start_time = time.time()
     log_reg_driver(images_train, labels_train, images_test, labels_test)
     print('Done Regression!')
+    end_time = time.time()
+    print("Elapsed time was %g minutes" % ((end_time - start_time)/60))
 
 
 if __name__ == '__main__':
